b1fee65848cabf05888229683a569890b395879b
CamiloParraS <juanparracsfn@gmail.com>
2026-03-02 11:56:21 -0500

    Ejericicio 01 Bloque 4

---
+++ Bloque_4/Ejercicio01/Codigo/README.md
@@ -0,0 +1,10 @@
+Crea una clase llamada Cuenta que tendrå los siguientes atributos: titular y cantidad(puede tener decimales).
+
+El titular sera obligatorio y la cantidad es opcional. Crea dos constructores que cumpla lo anterior.
+
+Crea sus métodos get, set y toString.
+
+Tendrå dos métodos especiales:
+- ingresar(double cantidad): se ingresa una cantidad a la cuenta, si la cantidad introducida es negativa, no se harå nada.
+
+- retirar(double cantidad): se retira una cantidad a la cuenta, si restando la cantidad actual a la que nos pasan es negativa, la cantidad de la cuenta pasa a ser 0.
---
+++ Bloque_4/Ejercicio01/Codigo/src/Cuenta.java
@@ -0,0 +1,74 @@
+public class Cuenta {
+
+    // === Atributos ===
+    private String titular;
+    private double cantidad;
+
+    // === Constructores ===
+    // Solo Titular
+    public Cuenta(String titular) {
+        this.titular = titular;
+        this.cantidad = 0.0;
+    }
+
+    // Titular y Cantidad
+    public Cuenta(String titular, double cantidad) {
+        this.titular = titular;
+        this.cantidad = cantidad; 
+    }
+
+    // === Metodos ===
+
+    // getters
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    // setters
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    // ingresar
+    public void ingresar(double cantidad) {
+        if (cantidad > 0) {
+            this.cantidad += cantidad;
+        }
+    }
+
+    // retirar
+    public void retirar(double cantidad) {
+        this.cantidad -= cantidad;
+        if (this.cantidad < 0) {
+            this.cantidad = 0;
+        }
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "titular = '" + titular + "\'" + ", cantidad = " + cantidad;
+    }
+
+    public static void main(String[] args) {
+        Cuenta c1 = new Cuenta("Carlitos");
+        System.out.println(c1);
+
+        c1.ingresar(200.0);
+        System.out.println(c1);
+
+        c1.ingresar(-100.0);
+        System.out.println(c1);
+
+        c1.retirar(600.0);
+        System.out.println(c1);
+    }
+}
