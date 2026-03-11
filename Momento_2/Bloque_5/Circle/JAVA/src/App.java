3354c9e2eb6131884125a1523588970ac1939179
CamiloParraS <juanparracsfn@gmail.com>
2026-03-11 00:07:29 -0500

    Completed the first excercise from Bloque_5 in JAVA

---
+++ Momento_2/Bloque_5/Circle/JAVA/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Circle circle = new Circle(2.0, "blue");
+        System.out.println(circle);
+        System.out.println("Area: " + circle.getArea());
+
+        System.out.println("-----------------------------");
+        System.out.println("CYLINDER");
+        Cylinder cylinder = new Cylinder(2.0, 5.0, "green");
+        System.out.println(cylinder);
+        System.out.println("Volume: " + cylinder.getVolume());
+    }
+}
